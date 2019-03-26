f90f1ba07a5d1db74dda35e60b872af34be06129
4000D <dkdkajej@gmail.com>
2019-03-26 18:02:00 +0900

    add gas reporter

--- truffle-config.js
+++ truffle-config.js
@@ -12,5 +12,14 @@ module.exports = {
       port: 8547,
       network_id: '*', // eslint-disable-line camelcase
     },
-  }
+  },
+  mocha: {
+    reporter: 'eth-gas-reporter',
+    reporterOptions: {
+      currency: 'USD',
+      gasPrice: 21,
+    },
+    useColors: true,
+    enableTimeouts: false,
+  },
 };
