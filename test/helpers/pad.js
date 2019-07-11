2d87d558e64257b94242b254c46581499cd3f777
4000D <dkdkajej@gmail.com>
2019-07-11 18:38:34 +0900

    test: update helper to web3@1.x

--- test/helpers/pad.js
+++ test/helpers/pad.js
@@ -2,14 +2,14 @@ const { marshalString, unmarshalString } = require('./marshal');
 
 const DEFAULT_PAD_LENGTH = 2 * 32;
 
-function padLeft (str, pad_length = DEFAULT_PAD_LENGTH) {
-  const v = web3.toHex(str);
-  return marshalString(web3.padLeft(unmarshalString(v), pad_length));
+function padLeft (str, padLength = DEFAULT_PAD_LENGTH) {
+  const v = web3.utils.toHex(str);
+  return marshalString(web3.utils.padLeft(unmarshalString(v), padLength));
 }
 
-function padRight (str, pad_length = DEFAULT_PAD_LENGTH) {
-  const v = web3.toHex(str);
-  return marshalString(web3.padRight(unmarshalString(v), pad_length));
+function padRight (str, padLength = DEFAULT_PAD_LENGTH) {
+  const v = web3.utils.toHex(str);
+  return marshalString(web3.utils.padRight(unmarshalString(v), padLength));
 }
 
 module.exports = {
