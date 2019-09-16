65e95062c376377b4c47b0973f8fb2346df3520b
Jin <jin.s@onther.io>
2019-09-16 15:43:08 +0900

    migration: modified deployer

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,16 +1,8 @@
-const RequestableERC20Wrapper = artifacts.require('./RequestableERC20Wrapper.sol');
-const ERC20Mintable = artifacts.require('./ERC20Mintable.sol');
+const RequestableERC20 = artifacts.require('./RequestableERC20.sol');
 
 const development = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test';
-const tokenAddress = process.env.TOKEN;
 
 module.exports = function (deployer) {
-  if (tokenAddress) {
-    deployer.deploy(RequestableERC20Wrapper, development, tokenAddress);
+    deployer.deploy(RequestableERC20, development);
     return;
-  }
-
-  deployer.deploy(ERC20Mintable).then((token) =>
-    deployer.deploy(RequestableERC20Wrapper, development, token.address)
-  ).catch(e => { throw e; });
 };
