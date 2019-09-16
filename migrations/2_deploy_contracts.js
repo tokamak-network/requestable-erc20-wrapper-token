ff5b3b121e7564fec96336581697a1a6eb01d579
Jin <jin.s@onther.io>
2019-09-17 02:53:08 +0900

    migration: Combine ERC20 & ERC20Wrapper migrate scripts.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,20 @@
 const RequestableERC20 = artifacts.require('./RequestableERC20.sol');
+const RequestableERC20Wrapper = artifacts.require('./RequestableERC20Wrapper.sol');
+const ERC20Mintable = artifacts.require('./ERC20Mintable.sol');
 
 const development = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test';
+const tokenAddress = process.env.TOKEN;
 
 module.exports = function (deployer) {
-    deployer.deploy(RequestableERC20, development);
+  deployer.deploy(RequestableERC20, development);
+
+  if (tokenAddress) {
+    deployer.deploy(RequestableERC20Wrapper, development, tokenAddress);
     return;
+  }
+
+  deployer.deploy(ERC20Mintable).then((token) =>
+    deployer.deploy(RequestableERC20Wrapper, development, token.address)
+  ).catch(e => { throw e; });
+
 };
