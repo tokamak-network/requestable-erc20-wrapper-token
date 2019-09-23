bdfb67af94ee98fadb816534bba5ac96c614a8f6
4000D <dkdkajej@gmail.com>
2019-09-23 22:56:37 +0900

    migrations: update deploy script and README

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,20 +2,41 @@ const BN = require('bn.js');
 
 const RequestableERC20 = artifacts.require('./RequestableERC20.sol');
 const RequestableERC20Wrapper = artifacts.require('./RequestableERC20Wrapper.sol');
-const RequestableERC20Mintable = artifacts.require('./RequestableERC20Mintable.sol');
+const RequestableERC20Mintable = artifacts.require('./RequestableERC20MintableMock.sol');
+const RequestableERC20Burnable = artifacts.require('./RequestableERC20BurnableMock.sol');
+const RequestableERC20MintableBurnable = artifacts.require('./RequestableERC20MintableBurnableMock.sol');
 
-const development = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test';
+const development = process.env.DEVELOPMENT || false;
 const lockInRootChain = process.env.LOCK_IN_ROOTCHAIN === 'true';
-const initialSupply = process.env.LOCK_IN_ROOTCHAIN || '0';
+const initialSupply = process.env.INITIAL_SUPPLY || '0';
 const tokenAddress = process.env.TOKEN;
 
-module.exports = function (deployer) {
+const mintable = process.env.MINTABLE || false;
+const burnable = process.env.BURNABLE || false;
+
+module.exports = async function (deployer) {
+  console.log(`
+  Deployment parameters
+    development     ${development}
+    lockInRootChain ${Boolean(lockInRootChain)}
+    initialSupply   ${initialSupply}
+    tokenAddress    ${tokenAddress || '0x'}
+    mintable        ${Boolean(mintable)}
+    burnable        ${Boolean(burnable)}
+`);
+
   if (tokenAddress) {
-    deployer.deploy(RequestableERC20Wrapper, development, lockInRootChain, initialSupply, tokenAddress);
+    console.log(`Deploying RequestableERC20Wrapper with token ${tokenAddress}`);
+    await deployer.deploy(RequestableERC20Wrapper, development, lockInRootChain, tokenAddress);
     return;
   }
 
-  deployer.deploy(RequestableERC20Mintable, development, lockInRootChain).then((token) =>
-    deployer.deploy(RequestableERC20Wrapper, development, lockInRootChain, token.address)
-  ).catch(e => { throw e; });
+  const deployToken = () => {
+    if (mintable && burnable) return deployer.deploy(RequestableERC20MintableBurnable, development, lockInRootChain);
+    if (mintable) return deployer.deploy(RequestableERC20Mintable, development, lockInRootChain);
+    if (burnable) return deployer.deploy(RequestableERC20Burnable, development, lockInRootChain, initialSupply);
+    return deployer.deploy(RequestableERC20, development, lockInRootChain, initialSupply);
+  };
+
+  const token = await deployToken();
 };
